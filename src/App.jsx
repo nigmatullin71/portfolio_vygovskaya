d7375454dbfa634597f377b7505a77dc97846a1e
nigmatullin,gr <nigmatullin.gr@gmail.com>
2024-12-24 06:43:58 +0300

    first

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,9 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Skill from "./components/Skill";
-import Projects from "./components/Projects";
 import Contact from "./components/Contact";
+import Projects from "./components/Projects";
+
 
 const App = () => {
     return (
